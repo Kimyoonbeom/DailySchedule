9552668b42669f7c762f92509ca1af55770d1150
Kimyoonbeom <kimybeom@naver.com>
2025-03-24 20:05:16 +0900

    중간 저장 5
    add: ScheduleService, ScheduleController 시작.

--- src/main/java/com/example/dailyschedule/Controller/ScheduleController.java
+++ src/main/java/com/example/dailyschedule/Controller/ScheduleController.java
@@ -1,19 +1,30 @@
 package com.example.dailyschedule.Controller;
 
+import com.example.dailyschedule.Entity.ScheduleEntity;
 import com.example.dailyschedule.Service.ScheduleService;
+import com.example.dailyschedule.dto.ScheduleRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/schedules")
 public class ScheduleController {
 
     @Autowired
     private ScheduleService scheduleService;
 
     @PostMapping
-    public ResponseEntity<String> createSchedule(@RequestBody Schedule daily_schedule){
-        scheduleService.createSchedule(daily_schedule);
+    public ResponseEntity<String> createSchedule(@RequestBody ScheduleRequestDto request){
+        ScheduleEntity schedule = new ScheduleEntity(
+                request.getTask(),
+                request.getAuthorName(),
+                request.getPassword()
+        );
+        scheduleService.createSchedule(schedule);
         return ResponseEntity.ok("일정이 생성되었습니다.");
     }
 }
--- src/main/java/com/example/dailyschedule/Service/ScheduleService.java
+++ src/main/java/com/example/dailyschedule/Service/ScheduleService.java
@@ -1,4 +1,19 @@
 package com.example.dailyschedule.Service;
 
+import com.example.dailyschedule.Entity.ScheduleEntity;
+import com.example.dailyschedule.Repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ScheduleService {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    public void createSchedule(ScheduleEntity schedule){
+        scheduleRepository.saveSchedule(schedule);
+    }
+
+    public
 }
